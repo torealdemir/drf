c0eba7a63dc1ff09b52a4f0db48528fdbaf4d8e9
torealdemir <aldemirtore@gmail.com>
2023-10-24 13:10:12 +0300

    some commit

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from blog.models import Post, Category
+from django.contrib.auth.models import User
+
+class PostTests(APITestCase):
+    def test_view_posts(self):
+
+        url = reverse('blog_api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 # Create your tests here.
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'blog_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +126,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+        
+    ]
+}
